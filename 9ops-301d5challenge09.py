cc62e7312c7ebf6b992d130ad531689b45e78594
R00sterGuy <alexander88echols@gmail.com>
2022-09-09 22:12:15 +0000

    lists are fun

---
+++ 9ops-301d5challenge09.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+# Script: Ops 301 Class 09 Ops Challenges: Bash in Python
+# Author: Alexander Echols.                    
+# Date of latest revision: 06 Sep 2022.      
+# Purpose: Write a Python script that works with lists
+
+# Main
+# Create a list
+myDesk = ['Soda', 'tea', 'water', 'classes', 'phone', 'medicine', 'Keys', 'Pizza', 'nuts', 'chocolate']
+# Print the fourth element of the list
+print(myDesk[3])
+print(myDesk[5:11])
+myDesk[6] = "onion"
+print(myDesk[6])
