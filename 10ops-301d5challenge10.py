ef816c295c2b39afb29c17249919fc6f47842cc5
R00sterGuy <alexander88echols@gmail.com>
2022-09-13 02:30:23 +0000

    I misunderstood, will adjust later

---
+++ 10ops-301d5challenge10.py
@@ -0,0 +1,5 @@
+#!/bin/python3
+# Script: Ops 301 Class 06 Ops Challenges: Directory Creation
+# Author: Alexander Echols.                    
+# Date of latest revision: 06 Sep 2022.      
+# Purpose: Write a Python  script that
